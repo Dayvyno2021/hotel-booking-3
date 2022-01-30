ecc1561db4cca4e3d7c980900ead4355de7ab074
Dayvyno2021 <dayvynomern2021@gmail.com>
2022-01-30 06:47:00 +0100

    resolved __dirname error

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use('/api/user', userRoute)
 app.use('/api', stripeRoute)
 app.use('/api', hotelRoutes)
 
-const __dirname = path.resolve
+const __dirname = path.resolve()
 if (process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname, '/frontend/build')))
   app.get('*', (req, res)=>res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
